8811d60a7226435d2ca16a004b931fe3cbdc7a10
Jeff Ahking <jeffahking@gmail.com>
2016-08-16 13:20:25 -0400

    heroku update

--- router/routes/home.js
+++ router/routes/home.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const pgp = require('pg-promise')();
 const db = require('../../db/db');
-const database = pgp('postgres://Wolphox@localhost:5432/graffiti');
+//const database = pgp('postgres://Wolphox@localhost:5432/graffiti');
 const database = pgp(process.env.DATABASE_URL);
 
 router.get('/', function (req, res){
